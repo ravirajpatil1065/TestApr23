e441afd1b6730d63c25381501acc67f6b3d61ab6
Raviraj Patil <ravirajpatil1065@gmail.com>
2023-05-23 08:39:05 +0530

    Update in StringPrac file

--- src/basicConcepts/StringPrac.java
+++ src/basicConcepts/StringPrac.java
@@ -4,6 +4,6 @@ public class StringPrac {
 	
 	public static void main(String[] args) {
 		System.out.println("I am in main method.. of branch A");
-		System.out.println("Creating conflict scenario from A");
+		System.out.println("Conflict resolved.");
 	}
 }
