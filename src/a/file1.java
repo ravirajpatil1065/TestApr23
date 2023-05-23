e966bab3ec659adb1be0c92f8391cc786851f87a
Raviraj Patil <ravirajpatil1065@gmail.com>
2023-05-23 08:53:48 +0530

    Conflict scen 2 from B2

--- src/a/file1.java
+++ src/a/file1.java
@@ -4,5 +4,7 @@ public class file1 {
 	
 	public static void main(String[] args) {
 		System.out.println("In branch a");
+		System.out.println("Creating conflist scenario 2");
+		System.out.println("This is from branch B2.");
 	}
 }
